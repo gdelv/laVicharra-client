cdcf01c11c7a601b9e78ad44ca14f205dc7098e7
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-06-25 01:59:28 -0400

    added bulma and data for menu

---
+++ src/data/index.js
@@ -0,0 +1,388 @@
+export const menu = {
+    appetizers: {
+        title: 'Cold Appetizers - Entradas Frias',
+        menu: [
+            {
+                name: 'Papa a la Huancaína o Ocopa',
+                price: '$9',
+                description: 'Sliced potato in creamy cheese sauce with Peruvian spices'
+            },
+            {
+                name: 'Palta Rellena',
+                price: '$9',
+                description: 'Stuffed avocado'
+            },
+            {
+                name: 'Causa Rellena de Pollo',
+                price: '$9',
+                description: 'Peruvian style mashed potatoes filled with chicken salad'
+            },
+            {
+                name: 'Choros a la Chalaca',
+                price: '$12',
+                description: 'Mussels topped with onions, cilantro, lemon & hot pepper'
+            },
+            {
+                name: 'Tiradito',
+                price: '$16',
+                description: 'Sliced fish fillet in a spicy sauce'
+            },
+            {
+                name: 'Leche de Tigre',
+                price: '$10',
+                description: 'Seafood cocktail in special lemon juice'
+            },
+            {
+                name: 'Ceviche de Pescado',
+                price: '$14',
+                description: 'Diced fish fillet marinated in lime juice and Peruvian peppers'
+            },
+            {
+                name: 'Ceviche Mixto',
+                price: '$16',
+                description: 'Diced fish fillet marinated in lime juice and Peruvian peppers with octopus, calamari & shrimp'
+            },
+            {
+                name: 'Ceviche de Camarones',
+                price: '$16',
+                description: 'Shrimp marinated in lime juice and Peruvian peppers'
+            }
+        ]
+    }, 
+    sideDishes: {
+        title: 'Small Dishes - Piqueos Criollos',
+        menu: [
+            {
+                name: 'Anticucho',
+                price: '$12',
+                description: 'Six pieces of marinated & grilled veal heart served with Peruvian corn and golden roasted potatoes'
+            },
+            {
+                name: 'Anticucho & Rachi',
+                price: '$13',
+                description: 'Anticucho accompanied with grilled tripe'
+            },
+            {
+                name: 'Papa Rellena',
+                price: '$8',
+                description: 'Mashed potato patty stuffed with chopped meat'
+            },
+            {
+                name: 'Salchipapas',
+                price: '$7',
+                description: 'Fried hot dog over french fries served with salad & special sauce'
+            },
+            {
+                name: 'Salchipollo',
+                price: '$10',
+                description: 'Fried chicken over french fries served with salad & special sauce'
+            },
+            {
+                name: 'Chicharrón de Pescado',
+                price: '$14',
+                description: 'Fried fish served with yucca & criolla red onion salad'
+            },
+            {
+                name: 'Chicharrón de Calamares',
+                price: '$15',
+                description: 'Fried calamari served with yucca & criolla red onion salad'
+            },
+            {
+                name: 'Yuca Frita con Crema',
+                price: '$6',
+                description: 'Fried yucca with huancaína or ocopa sauce'
+            },
+            {
+                name: 'Empanadas de Carne o Pollo',
+                price: '$2.50',
+                description: 'Beef or chicken empanadas'
+            }
+        ]
+    },
+    mainDishes: {
+        title: 'Main Course - Platos De Fondo',
+        menu: [
+            {
+                name: 'Pollada',
+                price: '$13',
+                description: 'Peruvian style fried chicken served with salad and french fries'
+            },
+            {
+                name: 'Milanesa de Pollo',
+                price: '$16',
+                description: 'Breaded chicken cutlet served with rice, salad & french fries'
+            },
+            {
+                name: 'Milanesa de Carne',
+                price: '$16',
+                description: 'Breaded beef cutlet served with rice, salad & french fries'
+            },
+            {
+                name: 'Lomo Saltado',
+                price: '$14',
+                description: 'Sautéed beef strips with onion, tomato, rice & french fries'
+            },
+            {
+                name: 'Pollo Saltado',
+                price: '$14',
+                description: 'Sautéed chicken strips with onion, tomato, rice & french fries'
+            },
+            {
+                name: 'Aeropuerto con Carne o Pollo',
+                price: '$17',
+                description: 'Peruvian style fried rice and Chinese linguini with beef or chicken'
+            },
+            {
+                name: 'Arroz Chaufa de Pollo o Carne',
+                price: '$14',
+                description: 'Peruvian style fried rice with chicken or beef'
+            },
+            {
+                name: 'Arroz Chaufa de Mariscos',
+                price: '$16',
+                description: 'Peruvian style fried rice with mixed seafood'
+            },
+            {
+                name: 'Tallarin a la Huancaína con Pollo o Carne a la Parilla',
+                price: '$16',
+                description: 'Spaghetti in yellow pepper sauce with grilled steak or chicken'
+            },
+            {
+                name: 'Tallarin Verde con Bistec o Pollo a la Parrilla',
+                price: '$16',
+                description: 'Spaghetti in Peruvian pesto sauce with grilled sirloin steak'
+            },
+            {
+                name: 'Tallarin Verde con Entraña',
+                price: '$21',
+                description: 'Spaghetti in Peruvian pesto sauce with grilled skirt steak'
+            },
+            {
+                name: 'Tallarin Saltado de Carne o Pollo',
+                price: '$16',
+                description: 'Spaghetti with sautéed strips of beef or chicken with onion & tomato'
+            },
+            {
+                name: 'Tallarin Saltado de Mariscos',
+                price: '$16',
+                description: 'Spaghetti with mixed seafood flambé'
+            },
+            {
+                name: 'Arroz con Mariscos',
+                price: '$18',
+                description: 'Rice with seafood'
+            },
+            {
+                name: 'Mojarra Frita con Ensalada & Yuca',
+                price: '$20',
+                description: 'Fried whole fish served with salad & yucca'
+            },
+            {
+                name: 'Sudado de Pescado',
+                price: '$18',
+                description: 'Steamed fish'
+            },
+            {
+                name: 'Jalea',
+                price: '$19 / $37',
+                description: 'Fried seafood combination for two or four'
+            },
+            {
+                name: 'Pescado a lo Macho',
+                price: '$18.50',
+                description: 'Fried fish fillet with seafood sauce, tomato & onion served with rice'
+            },
+            {
+                name: 'Filete de Pescado con Yuca & Salsa Criolla',
+                price: '$13',
+                description: 'Fish fillet accompanied with yucca & red onion criolla sauce'
+            },
+            {
+                name: 'Pargo Rojo Frito',
+                price: '$22',
+                description: 'Fried red snapper served with yucca & rice'
+            },
+            {
+                name: 'Parilladas',
+                price: '$17',
+                description: 'Grilled steak served with salad & golden potatoes'
+            },
+            {
+                name: 'Bistec a lo Pobre',
+                price: '$17',
+                description: 'Grilled sirloin steak topped with fried egg served with rice, french fries & fried plantains'
+            },
+            {
+                name: 'Tacu Tacu con Lomo al Jugo',
+                price: '$16',
+                description: 'Mixed rice & beans with sautéed beef strips'
+            },
+            {
+                name: 'Churrasco',
+                price: '$21',
+                description: 'Chuck steak served with french fries & salad'
+            }
+        ]
+    },
+    soups: {
+        title: 'Soups - Sopas',
+        menu: [
+            {
+                name: 'Chupe de Camarones',
+                price: '$18',
+                description: 'Milk-based shrimp soup with cheese'
+            },
+            {
+                name: 'Parihuela',
+                price: '$18',
+                description: 'Mixed seafood soup'
+            },
+            {
+                name: 'Sopa a la Minuta',
+                price: '$13',
+                description: 'Milk-based soup with ground beef & oregano'
+            },
+            {
+                name: 'Caldo de Gallina',
+                price: '$13',
+                description: 'Chicken soup'
+            },
+            {
+                name: 'Patasca',
+                price: '$13',
+                description: 'Corn-beef soup'
+            },
+
+        ]
+    },
+    desserts: {
+        title: 'Desserts - Postres',
+        menu: [
+            {
+                name: 'Alfajores',
+                price: '$2',
+                description: 'Dulce de leche cookies'
+            },
+            {
+                name: 'Leche Asada',
+                price: '$5',
+                description: 'Peruvian style "roasted milk" custard'
+            },
+            {
+                name: 'Picarones',
+                price: '$8',
+                description:'Peruvian style doughnut made from squash or sweet potato served with syrup'
+            },
+            {
+                name: 'Mazamorra Morada',
+                price: '$6',
+                description:'Sweet purple corn pudding'
+            },
+            {
+                name: 'Arroz con Leche',
+                price: '$8',
+                description: 'Rice pudding'
+            },
+            {
+                name: 'Churros (2)',
+                price: '$4 / $6',
+                description: 'Fried dough pastry covered in cinnamon sugar mixture'
+            },
+            {
+                name: 'Torta Helada',
+                price: '$6',
+                description:'A layer of yellow cake in between a gummy sheet of gelatin and a fluffy gelatin parfait'
+            }
+        ]
+    },
+    drinks: {
+        title: 'Drinks - Bebidas',
+        menu: [ 
+            {
+                name: 'Chicha Morada',
+                price: '$3.50 / $8',
+                description: 'Sweet and fruity purple corn drink'
+            },
+            {
+                name: 'Chicha de Jora',
+                price: '$8',
+                description: 'Traditional corn beer'
+            },
+            {
+                name: 'Cervezas',
+                price: '$5',
+                description: 'Beer'
+            },
+            {
+                name: 'Sangria',
+                price: '$8 / $28',
+                description: ''
+            },
+            {
+                name: 'Variedad de Gaseosas',
+                price: '$2',
+                description: 'Variety of sodas'
+            },
+            {
+                name: 'Café o Té',
+                price: '$2',
+                description: 'Coffee or tea'
+            },
+            {
+                name: 'Agua de Manzana',
+                price: '$1.50',
+                description: 'Peruvian style apple water'
+            }
+        ]
+    },
+    extras: {
+        title: 'Extras',
+        menu: [
+            {
+                name: 'Tostones',
+                price: '$6',
+                description: 'Twice-fried green plantain slices'
+            },
+            {
+                name: 'Maduros',
+                price: '$6',
+                description: 'Once-fried sweet plantains'
+            },
+            {
+                name: 'Frijoles',
+                price: '$5',
+                description: 'Beans'
+            },
+            {
+                name: 'Papas Fritas',
+                price: '$5',
+                description: 'French fries'
+            },
+            {
+                name: 'Camote Frito',
+                price: '$5',
+                description: 'Fried sweet potatoes'
+            },
+            {
+                name: 'Papas Asadas',
+                price: '$5',
+                description: 'Roasted potatoes'
+            },
+            {
+                name: 'Arroz Blanco',
+                price: '$5',
+                description: 'White rice'
+            },
+            {
+                name: 'Ensalada Vicharra',
+                price: '$5 / $9',
+                description: 'Vicharra salad'
+            },
+            {
+                name: 'Salsa Criolla',
+                price: '$5',
+                description: 'Red onion salad'
+            }
+        ]
+    }
+}
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -13,7 +13,6 @@ export default function Home() {
                     exact 
                     to='/menu'
                     activeClassName='active'>
-                    
                         Menu
                 </NavLink>
                 <NavLink
